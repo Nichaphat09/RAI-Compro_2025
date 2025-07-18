6d5f23425b55a291d40ef1eda933f720b0b137cd
Nichaphat09 <sriwong.enjoy@gmail.com>
2025-07-18 10:16:17 +0000

    complete lab-1

---
+++ Lab-1/ex02.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("My name is : %s\n" ,"Nichaphat Pemanukornrak");
+    printf("I am : %dyears old\n" , 19);
+    printf("I am : %fcm tall\n" , 155.00);
+    return 0;
+}
---
+++ Lab-1/ex05.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("\\Americz ya!\\");
+    return 0;
+}
