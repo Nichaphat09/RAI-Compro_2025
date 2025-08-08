e5164db21e61a58f2d08743b8f206415ac2adfb9
Nichaphat09 <sriwong.enjoy@gmail.com>
2025-08-08 14:03:28 +0000

    Lab3

---
+++ Lab3/ex01.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+ int num1 ,num2;
+
+ printf ("Enter a number: ");
+ scanf("%d",&num1);
+
+ printf ("Enter a number:");
+ scanf ("%d",&num2);
+ if (num1 != num2)
+ {
+      printf("Does not match. Try again\n");
+ }
+ else{
+     printf("Match\n");
+ } 
+
+    return 0;
+}
